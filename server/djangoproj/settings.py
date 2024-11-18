58e1aabac77c14acc30f98e379c5960b17cec301
bhanuprakash94 <venkatabhanuprakash.n@gmail.com>
2024-11-19 01:45:02 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -34,10 +34,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
-    "https://venkatabhan1-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
+    "https://venkatabhan1-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",  # noqa: E501
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://venkatabhan1-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    "https://venkatabhan1-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"  # noqa: E501
 ]
 
 REST_FRAMEWORK = {"DEFAULT_AUTHENTICATION_CLASSES": []}
